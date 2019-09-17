2df5e91027adde2ebbded47fd29976e9475dad6c
huuco <nguyen.huu.co@sun-asterisk.com>
2019-09-17 18:21:14 +0700

    add layout website admin

---
+++ spec/controllers/admin/base_controller.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe Admin::BaseController, type: :controller do
+  describe "GET #index" do
+    it "is expected render template" do
+      expect{response.to render_template :index}
+    end
+  end
+  
+end
